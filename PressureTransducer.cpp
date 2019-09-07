ae3e7ae5e65553f576c931825fd1c675d7cfc0c0
Max <maximilian.paulsen@gmail.com>
2019-09-07 16:14:55 -0700

    Flushed out packet genreated for PTs

--- PressureTransducer.cpp
+++ PressureTransducer.cpp
@@ -9,15 +9,22 @@ PressureTransducer::PressureTransducer(int id) : id(id){
 
 int PressureTransducer::writeValueToBuffer(char buffer[]){
   char packetId[2];
-  packetId[0] = 'P';
-  packetId[1] = '0';
   char data[5];
   int analogValue = analogRead(this->id);
   int originalAnalogValue = analogValue;
+
+  packetId[0] = 'P';
+  if(this->id < 10){
+    packetId[1] = '0' + this->id;
+  } else {
+    packetId[1] = 'A' + (this->id-10);
+  }
+
   for(int i = 0; i<5; i++){
     data[4-i] = '0' + analogValue%10;
     analogValue /= 10;
   }
+  
   SCMPacket packet(packetId, data);
   packet.write(buffer);
   return originalAnalogValue;
--- test/PressureTransducerTest.h
+++ test/PressureTransducerTest.h
@@ -29,19 +29,42 @@ namespace RPL {
     MU_TEST(pt_writes_accurate_packet_for_id_zero){
       Mocks::resetPins();
       char buf[20];
-      buf[12] = '\0';
+      ZERO_MEM(buf);
       const char * expectedPacket = "P0,00010,57;";
       PressureTransducer pt(0);
       Mocks::setAnalogWrite(0, 10);
       mu_assert_int_eq(10, pt.writeValueToBuffer(buf));
       mu_assert_string_eq(expectedPacket, buf);
+    }
 
+    MU_TEST(pt_writes_accurate_packet_for_id_one){
+      Mocks::resetPins();
+      char buf[20];
+      ZERO_MEM(buf);
+      const char * expectedPacket = "P1,00011,59;";
+      PressureTransducer pt(1);
+      Mocks::setAnalogWrite(1, 11);
+      mu_assert_int_eq(11, pt.writeValueToBuffer(buf));
+      mu_assert_string_eq(expectedPacket, buf);
+    }
+
+    MU_TEST(pt_writes_accurate_packet_for_id_eleven){
+      Mocks::resetPins();
+      char buf[20];
+      ZERO_MEM(buf);
+      const char * expectedPacket = "PB,01023,80;";
+      PressureTransducer pt(11);
+      Mocks::setAnalogWrite(11, 1023);
+      mu_assert_int_eq(1023, pt.writeValueToBuffer(buf));
+      mu_assert_string_eq(expectedPacket, buf);
     }
 
     MU_TEST_SUITE(pressure_transducer_test){
       MU_RUN_TEST(pt_reads_value_via_analog_read);
       MU_RUN_TEST(pt_discriminates_differnt_pins);
       MU_RUN_TEST(pt_writes_accurate_packet_for_id_zero);
+      MU_RUN_TEST(pt_writes_accurate_packet_for_id_one);
+      MU_RUN_TEST(pt_writes_accurate_packet_for_id_eleven);
     }
   }
 }
